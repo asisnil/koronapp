1933e224d29e363f23f61bf02ca98e131ad4477d
Ashraful Islam <asisnil@protonmail.com>
2021-06-28 16:56:11 +0600

    Added - about page, backup page, function to add to localstorage, function to display localstorage items,

--- js/js.js
+++ js/js.js
@@ -3,6 +3,10 @@ const addNote = document.querySelector(".add-note");
 const sortItems = document.querySelector(".sort-items");
 const tags = document.querySelector(".tag-items");
 const moreItems = document.querySelector(".more-items");
+const aboutItems = document.querySelector(".about-items");
+const backupItems = document.querySelector(".backup-items");
+
+
 const overLay = document.querySelector(".overlay");
 const cross = document.querySelector(".cross");
 const addBtn = document.getElementById("add-btn");
@@ -11,7 +15,8 @@ const themeBtn = document.getElementById("theme-btn");
 const sortBtn = document.getElementById("sort-btn");
 const moreBtn = document.getElementById("more-btn");
 const cancelBtn = document.getElementById("cancel");
-
+const aboutBtn = document.getElementById("about-page-btn");
+const backupBtn = document.getElementById("backup-btn");
 addBtn.onclick = function() {
   addNote.style.display = "flex";
   overLay.style.display = "block";
@@ -35,68 +40,27 @@ sortBtn.onclick = function() {
   cross.style.display = "flex";
   sortItems.style.opacity = "1";
 };
+aboutBtn.onclick = function() {
+  aboutItems.style.display = "flex";
+  overLay.style.display = "block";
+  cross.style.display = "flex";
+  aboutItems.style.opacity = "1";
+  moreItems.style.display = "none";
+};
+backupBtn.onclick = function() {
+  backupItems.style.display = "flex";
+  overLay.style.display = "block";
+  cross.style.display = "flex";
+  backupItems.style.opacity = "1";
+  moreItems.style.display = "none";
+};
 cancel.onclick = function(){
+  aboutItems.style.display = "none";
   addNote.style.display = "none";
   tags.style.display = "none";
   moreItems.style.display = "none";
   overLay.style.display = "none";
   cross.style.display = "none";
   sortItems.style.display = "none";
+  backupItems.style.display = "none";
 };
-
-/*Add note*/
-let addNoteBtn = document.querySelector(".newNote");
-function addNoteFunc(){
-
-  let date = new Date();
-  let d = date.getDate();
-  let m = date.getMonth();
-  let y = date.getFullYear();
-  m = m + 1;
-  let dates = d + "/" + m + "/" + y;
-
-  let notes = document.querySelector(".notes");
-  let titleValue = document.getElementById("add-note-input").value;
-  let textValue = document.getElementById("add-note-text").value;
-
-  let note = document.createElement("div");
-  note.classList.add('note');
-  notes.appendChild(note);
-  
-  let noteTitle = document.createElement("h3");
-  noteTitle.classList.add('note-title');
-  note.appendChild(noteTitle);
-  noteTitle.appendChild(document.createTextNode(titleValue));
-
-  let noteContent = document.createElement("p");
-  noteContent.classList.add('note-content');
-  note.appendChild(noteContent);
-  noteContent.appendChild(document.createTextNode(textValue));
-
-  let noteTime = document.createElement("p");
-  noteTime.classList.add('note-time');
-  note.appendChild(noteTime);
-  noteTime.appendChild(document.createTextNode(dates));
-
-
-  let noteDelete = document.createElement("button");
-  noteDelete.classList.add('note-delete');
-  note.appendChild(noteDelete);
-
-  let noteDeleteImg = document.createElement("img");
-  noteDeleteImg.src = "/resource/trash-2.svg";
-  noteDelete.appendChild(noteDeleteImg);
-
-  let noteObject = {
-    noteTitle : titleValue,
-    noteContent : textValue,
-    date : dates
-  }
-
-}
-
-
-let deleteItem = document.querySelector(".note-delete").parentElement;
-  function deleteNote(){
-  deleteItem.remove();
-  };
---
+++ js/note.js
@@ -0,0 +1,78 @@
+function addNewNoteFunction(){
+  /*Selectors*/
+  let noteTitleValue = document.getElementById('add-note-input').value;
+  let noteContentValue = document.getElementById('add-note-text').value;
+  let addNewNoteBtn = document.getElementById('add-new-note-btn');
+
+  /*Get time*/
+  let date = new Date();
+  let d = date.getDate();
+  let m = date.getMonth();
+  let y = date.getFullYear();
+  m = m + 1;
+  let dates = d + "/" + m + "/" + y;
+
+  /*Unique Id*/
+  function uniqueID() {
+  return Math.floor(Math.random() * Date.now())
+  }
+  let unique = "id" + uniqueID();
+
+  let noteObject = {
+    noteTitle : noteTitleValue,
+    noteContent : noteContentValue,
+    noteDate : dates,
+    noteId : unique
+  }
+  let noteJson = JSON.stringify(noteObject);
+  localStorage.setItem(unique, noteJson);
+}
+
+
+function getNotesFunction(){
+  var values = [];
+    let keys = Object.keys(localStorage);
+    let i = keys.length;
+
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]) );
+    };
+  let objValues = values.map(JSON.parse);
+  return objValues;
+}
+
+let allNotesArray = getNotesFunction();
+
+function displayNotes(){
+  let notesContainer = document.querySelector(".notes");
+  for(let i=0; i<allNotesArray.length;i++){
+
+  let note = document.createElement("div");
+  note.id = allNotesArray[i].noteId;
+  note.className = "note";
+  notesContainer.appendChild(note);
+
+  let noteTt = document.createElement("h3");
+  noteTt.className = "note-title";
+  noteTt.appendChild(document.createTextNode(allNotesArray[i].noteTitle));
+  note.appendChild(noteTt);  
+  
+  let noteCt = document.createElement("p");
+  noteCt.className = "note-content";
+  noteCt.appendChild(document.createTextNode(allNotesArray[i].noteContent));
+  note.appendChild(noteCt);
+
+  let noteTi = document.createElement("p");
+  noteTi.className = "note-time";
+  noteTi.appendChild(document.createTextNode(allNotesArray[i].noteDate));
+  note.appendChild(noteTi);
+
+  let noteDl = document.createElement("button");
+  noteDl.className = "note-delete";
+  let noteDlImg = document.createElement("img");
+  noteDlImg.src = "resource/trash-2.svg";
+  noteDl.appendChild(noteDlImg);
+  note.appendChild(noteDl);
+  }
+}
+displayNotes();
